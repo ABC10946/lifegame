dd1a34e548189b3842add345be8154f71ea0fd76
ABC10946 <alphalex10946@gmail.com>
2017-06-18 21:55:52 +0900

    [fix] curses crash from window size and field size are not matching.
    [update] for readable code,move LifeGame class to LifeGame.py.

---
+++ lifegame.py
@@ -0,0 +1,93 @@
+import copy
+
+class LifeGame():
+    def __init__(self):
+        self.step = 0
+        self.field = []
+
+    def gen_field(self,width,height):
+        self.width = width
+        self.height = height
+        self.field = [[False for x in range(width)] for y in range(height)]
+        self.zero_field = copy.deepcopy(self.field)
+        self.field_ = copy.deepcopy(self.field)
+
+    def life(self,x_pos,y_pos):
+        self.field[y_pos][x_pos] = True
+
+    def dead(self,x_pos,y_pos):
+        self.field[y_pos][x_pos] = False
+
+    def get_state(self,x_pos,y_pos):
+        return self.field[y_pos][x_pos]
+
+    def count_around_life(self,x_pos,y_pos):
+        around_life_count = 0
+        for y_delta in range(-1,2):
+            for x_delta in range(-1,2):
+                if (y_delta != 0 or x_delta != 0) and x_pos+x_delta >= 0 and y_pos+y_delta >= 0 and x_pos+x_delta < self.width and y_pos+y_delta < self.height:
+                    if self.field[y_pos+y_delta][x_pos+x_delta]:
+                        around_life_count += 1
+
+        return around_life_count
+
+
+    def next_step(self):
+        self.step += 1
+        around_life_count = 0
+        for y in range(self.height):
+            for x in range(self.width):
+                around_life_count = self.count_around_life(x,y)
+
+                if around_life_count == 3:
+                    self.field_[y][x] = True
+
+                elif around_life_count == 2:
+                    if self.field[y][x]:
+                        self.field_[y][x] = True
+                    else:
+                        self.field_[y][x] = False
+
+                elif around_life_count >= 4 or around_life_count <= 1:
+                    self.field_[y][x] = False
+
+        self.field = copy.deepcopy(self.field_)
+        self.field_ = copy.deepcopy(self.zero_field)
+
+    def output_field(self):
+        return "\n".join(["".join([str(int(x)) for x in y]) for y in self.field])
+
+    def output_count_life_field(self):
+        print("COUNT LIFE FIELD")
+        count_life_field = [list([0 for x in range(self.width)]) for y in range(self.height)]
+        for y in range(1,self.height-1):
+            for x in range(1,self.width-1):
+                count_life_field[y][x] = self.count_around_life(x,y)
+
+        return "\n".join(["".join([str(int(x)) for x in y]) for y in count_life_field])
+
+    def output_step(self):
+        return self.step
+
+    def clear_field(self):
+        self.field = copy.deepcopy(self.zero_field)
+
+    def save_field(self,filename):
+        f = open(filename,"wb")
+        pickle.dump(self.field,f)
+        f.close()
+        return 0
+
+    def load_field(self,filename):
+        try:
+            f = open(filename,"rb")
+            temp_field = pickle.load(f)
+            temp_field_width = len(temp_field[0])
+            temp_field_height = len(temp_field)
+
+            self.gen_field(temp_field_width,temp_field_height)
+            self.field = temp_field
+            return 0
+
+        except FileNotFoundError:
+            return -1
--- main.py
+++ main.py
@@ -1,98 +1,7 @@
 import sys
-import copy
 import curses
 import pickle
-
-class LifeGame():
-    def __init__(self):
-        self.step = 0
-        self.field = []
-
-    def gen_field(self,width,height):
-        self.width = width
-        self.height = height
-        self.field = [[False for x in range(width)] for y in range(height)]
-        self.zero_field = copy.deepcopy(self.field)
-        self.field_ = copy.deepcopy(self.field)
-
-    def life(self,x_pos,y_pos):
-        self.field[y_pos][x_pos] = True
-
-    def dead(self,x_pos,y_pos):
-        self.field[y_pos][x_pos] = False
-
-    def get_state(self,x_pos,y_pos):
-        return self.field[y_pos][x_pos]
-
-    def count_around_life(self,x_pos,y_pos):
-        around_life_count = 0
-        for y_delta in range(-1,2):
-            for x_delta in range(-1,2):
-                if (y_delta != 0 or x_delta != 0) and x_pos+x_delta >= 0 and y_pos+y_delta >= 0 and x_pos+x_delta < self.width and y_pos+y_delta < self.height:
-                    if self.field[y_pos+y_delta][x_pos+x_delta]:
-                        around_life_count += 1
-
-        return around_life_count
-
-
-    def next_step(self):
-        self.step += 1
-        around_life_count = 0
-        for y in range(self.height):
-            for x in range(self.width):
-                around_life_count = self.count_around_life(x,y)
-
-                if around_life_count == 3:
-                    self.field_[y][x] = True
-
-                elif around_life_count == 2:
-                    if self.field[y][x]:
-                        self.field_[y][x] = True
-                    else:
-                        self.field_[y][x] = False
-
-                elif around_life_count >= 4 or around_life_count <= 1:
-                    self.field_[y][x] = False
-
-        self.field = copy.deepcopy(self.field_)
-        self.field_ = copy.deepcopy(self.zero_field)
-
-    def output_field(self):
-        return "\n".join(["".join([str(int(x)) for x in y]) for y in self.field])
-
-    def output_count_life_field(self):
-        print("COUNT LIFE FIELD")
-        count_life_field = [list([0 for x in range(self.width)]) for y in range(self.height)]
-        for y in range(1,self.height-1):
-            for x in range(1,self.width-1):
-                count_life_field[y][x] = self.count_around_life(x,y)
-
-        return "\n".join(["".join([str(int(x)) for x in y]) for y in count_life_field])
-
-    def output_step(self):
-        return self.step
-
-    def clear_field(self):
-        self.field = copy.deepcopy(self.zero_field)
-
-    def save_field(self,filename):
-        f = open(filename,"wb")
-        pickle.dump(self.field,f)
-        f.close()
-        return 0
-
-    def load_field(self,filename):
-        try:
-            f = open(filename,"rb")
-            temp_field = pickle.load(f)
-            temp_field_width = len(temp_field[0])
-            temp_field_height = len(temp_field)
-
-            self.gen_field(temp_field_width,temp_field_height)
-            self.field = temp_field
-
-        except FileNotFoundError:
-            return -1
+from lifegame import *
 
 
 def main():
@@ -111,6 +20,8 @@ def main():
         file_handler = lifegame.load_field(f_name)
         if file_handler == 0:
             print("File load successful.")
+            width = lifegame.width
+            height = lifegame.height
         elif file_handler == -1:
             print("File not found.")
             main_loop_flag = False
@@ -120,19 +31,26 @@ def main():
         print("python main.py <width> <height>")
         print("python main.py <filename>")
 
-    if main_loop_flag:
+    screen = curses.initscr()
+    screen_height,screen_width = screen.getmaxyx()
+    if screen_height <= height or screen_width <= width:
+        print("Window size is too small to show field. (" + str(screen_width-1) + "," + str(screen_height-2) + ")")
+        main_loop_flag = False
 
-        screen = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
+    curses.noecho()
+    curses.cbreak()
+
+
+    if main_loop_flag:
         x = 0
-        y = 0
+        y = 1
         playFlag = False
         while True:
             screen.timeout(100)
             screen.clear()
             screen.addstr(0,0,"STEP:"+str(lifegame.output_step())+",play:"+("True" if playFlag else "False"))
-            screen.addstr(1,0,lifegame.output_field())
+            screen.addstr(1,0,"q to escape program,s to next_step,hjkl to move cursor,c to clear field,o to change cell state,w to save field")
+            screen.addstr(2,0,lifegame.output_field())
             screen.addstr(y+1,x,"x")
             c = screen.getch()
             if c == ord("q"):
@@ -146,7 +64,7 @@ def main():
                 if y < lifegame.height-1:
                     y += 1
             elif c == ord("k"):
-                if y > 0:
+                if y > 1:
                     y -= 1
             elif c == ord("l"):
                 if x < lifegame.width-1:
@@ -156,19 +74,19 @@ def main():
             elif c == ord("p"):
                 playFlag = False if playFlag else True
             elif c == ord("o"):
-                if lifegame.get_state(x,y):
-                    lifegame.dead(x,y)
+                if lifegame.get_state(x,y-1):
+                    lifegame.dead(x,y-1)
                 else:
-                    lifegame.life(x,y)
+                    lifegame.life(x,y-1)
             elif c == ord("w"):
                 lifegame.save_field("save.dump")
 
             if playFlag:
                 lifegame.next_step()
 
-        curses.noecho()
-        curses.echo()
-        curses.endwin()
+    curses.noecho()
+    curses.echo()
+    curses.endwin()
 
 if __name__ == "__main__":
     main()
